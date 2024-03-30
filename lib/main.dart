67ed5f0c5778f1fe820c9d841278c781d88527da
No-precinho <lfasfrassi@sga.pucminas.br>
2024-03-29 23:58:15 -0300

    autores

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,10 @@
+/// @luix-F - Luiz Fernando Antunes da Silva Frassi
+/// @RAFAEL2305003 - Rafael Rodrigues Oliveira
+/// @yurideoliveira - Yuri de Oliveira Silva
+/// @caioronan - Caio Ronan Horta
+/// 
+/// Projeto - @No-precinho
+library;
 import 'package:flutter/material.dart';
 import './StepForm.dart';
 
